5da47040ffa969bcdd45204a04d51804227fec77
Tiffany Jalovec <carr.tiffany@gmail.com>
2020-01-18 16:40:32 -0700

    save function

--- database/index.js
+++ database/index.js
@@ -13,11 +13,25 @@ let repoSchema = mongoose.Schema({
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (function (err, ) {
+let save = (data) {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+  var row = new Repo({
+    id: data.id,
+    name: data.name,
+    owner_id: data.owner_id,
+    stargazers_count: data.stargazers_count
+  });
 
-});
+  row.save(function(err, data) {
+    if (err) {
+      console.log('error in save', err);
+    } else {
+      console.log('sent to mongoose!');
+    }
+  })
+
+};
 
 module.exports.save = save;
